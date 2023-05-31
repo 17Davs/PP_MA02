7b368e4a2ed530b3811db3cbe866176eec157c13
David <davsantos17d.s@gmail.com>
2023-05-31 22:40:45 +0100

    ãtualização -> nome dos alunos e eliminação de ficheiros

--- PP_Project/src/editions/Edition.java
+++ PP_Project/src/editions/Edition.java
@@ -4,7 +4,7 @@
  */
 package editions;
 
-import editions.Contracts.EditionContract;
+
 import java.util.Arrays;
 import ma02_resources.project.Project;
 import ma02_resources.project.Submission;
@@ -13,7 +13,7 @@ import ma02_resources.project.Submission;
  *
  * @author David Santos
  */
-public class Edition implements EditionContract {
+public class Edition {
 
     private static final int initialSize = 5;
     private static int idCounter = 0;
@@ -30,12 +30,8 @@ public class Edition implements EditionContract {
         this.projects = new Project[size];
     }
 
-    @Override
-    public void addProject(Project p) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
+    
+   
     public int getNumberOfProjects() {
         int counter = 0;
         for (Project p : projects){
@@ -46,36 +42,6 @@ public class Edition implements EditionContract {
         return counter;
     }
 
-    @Override
-    public String progressToString() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public void addSubmission(Submission s) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    /**
-     * Comparison between an object and an Edition using the id
-     * @param obj Object to compare
-     * @return {@code true} if they have same id or {@code false} otherwise;
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Edition other = (Edition) obj;
-        
-        return this.id == other.id; //&& Arrays.deepEquals(this.projects, other.projects); //////perguntar se necessario verificar se é o mesmo array
-    }
 
       
 }
