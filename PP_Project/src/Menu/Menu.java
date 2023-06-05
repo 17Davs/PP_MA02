1baf0af54e8bd9a2eba10f84b490c9abea205738
17Davs <davsantos17d.s@gmail.com>
2023-06-05 12:54:10 +0100

    melhorias nos menus

--- PP_Project/src/Menu/Menu.java
+++ PP_Project/src/Menu/Menu.java
@@ -30,6 +30,8 @@ import pack.StudentImp;
  */
 public class Menu {
 
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "admin";
     private CBL cbl;
     private ParticipantsManager pm;
     private Participant loggedInParticipant;
@@ -42,44 +44,112 @@ public class Menu {
     }
 
     public void start() {
-        login();
-        showEditionsMenu();
-    }
+        int option = 0;
+        do {
+            System.out.println("=== Menu ===");
+            System.out.println("1. Log in");
+            System.out.println("2. Register");
+            System.out.println("3. Log in as Administrator");
+            System.out.println("4. Exit");
+            System.out.print("Option: ");
+            option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    if (login()) {
+                        showEditionsMenu();
+                    }
+                    break;
+                case 2:
+                    //register();
+                    break;
 
-    private void login() {
+                case 3:
+                    if (loginAdmin()) {
+                        // showAdminMenu();
+                    }
+                case 4:
+                    break;
+                default:
+                    System.out.println("Invalid option!");
+                    start();
+            }
+
+        } while (option != 4);
+
+    }
+//fazer com do while
+    private boolean login() {
+        int counter = 0;
         System.out.println("===== Login =====");
         System.out.print("Email: ");
+        scanner.nextLine();
         String email = scanner.nextLine();
         try {
             loggedInParticipant = pm.getParticipant(email);
             System.out.println("Login successful. Welcome, " + loggedInParticipant.getName() + "!");
+            return true;
         } catch (IllegalArgumentException e) {
             System.out.println("User not found");
-            login();
+            if (++counter < 3) {
+                login();
+            } 
+            return false;
         }
+    }
+
+    private boolean loginAdmin() {
+        int counter = 0;
+
+        System.out.println("===== Login =====");
+        System.out.print("Email: ");
+        scanner.nextLine();
+        String email = scanner.nextLine();
+        if (email.equals(USERNAME)) {
+            do {
+                System.out.print("Password: ");
+                String password = scanner.nextLine();
+                if (!password.equals(PASSWORD)) {
+                    System.out.println("Login Failed!");
+                } else {
+                return true;
+                }
+            } while (++counter < 3);
+                
+        } else {
+            System.out.println("Login Failed!");
+                return false;
+        }
+        return false;
     }
 
     private void showEditionsMenu() {
         System.out.println("===== Editions Menu =====");
         System.out.println("Select an edition:");
+        try {
 
-        Edition[] editions = cbl.getEditionsByParticipant(loggedInParticipant);
-        for (int i = 0; i < editions.length; i++) {
-            System.out.println((i + 1) + ". " + editions[i].getName());
+            Edition[] editions = cbl.getEditionsByParticipant(loggedInParticipant);
+
+            for (int i = 0; i < editions.length; i++) {
+                System.out.println((i + 1) + ". " + editions[i].getName());
+            }
+
+            System.out.print("Enter the number of the edition: ");
+            int editionNumber = scanner.nextInt();
+            scanner.nextLine();
+
+            // Verifique se o número da edição é válido
+            if (editionNumber >= 1 && editionNumber <= editions.length) {
+                Edition selectedEdition = editions[editionNumber - 1];
+                showProjectsMenu(selectedEdition);
+            } else {
+                System.out.println("Invalid selection. Please try again.");
+                showEditionsMenu();
+            }
+        } catch (NullPointerException e) {
+            System.out.println(e.getMessage());
+
         }
 
-        System.out.print("Enter the number of the edition: ");
-        int editionNumber = scanner.nextInt();
-        scanner.nextLine();
-
-        // Verifique se o número da edição é válido
-        if (editionNumber >= 1 && editionNumber <= editions.length) {
-            Edition selectedEdition = editions[editionNumber - 1];
-            showProjectsMenu(selectedEdition);
-        } else {
-            System.out.println("Invalid selection. Please try again.");
-            showEditionsMenu();
-        }
     }
 
     private void showProjectsMenu(Edition edition) {
@@ -142,10 +212,10 @@ public class Menu {
         // Crie uma instância da classe CBL e adicione participantes, edições, projetos, etc.
         ParticipantsManager pm = new ParticipantsManager();
         CBL cbl = new CBLImp();
-        
+
         Contact c = new ContactImp("Street", "city", "state", "zipCode", "country", "phone");
         Instituition estg = new InstituitionImp("ESTG", "estg@estg.ipp.pt", "estg.ipp.pt", "Escola de Tecnologia e Gestão de Felgueiras", c, InstituitionType.UNIVERSITY);
-        Student p1 = new StudentImp(8220651, "David Santos", "david@estg.ipp.pt", c, estg); 
+        Student p1 = new StudentImp(8220651, "David Santos", "david@estg.ipp.pt", c, estg);
         try {
             pm.addParticipant(p1);
         } catch (AlreadyExistsInArray ex) {
