fa205fdf765553392b491feabc4a13b43775c5a3
Carolina <8180091@estg.ipp.pt>
2023-06-09 00:22:55 +0100

    partially made javadoc ProjectImp.java

--- PP_Project/src/CBLStructure/ProjectImp.java
+++ PP_Project/src/CBLStructure/ProjectImp.java
@@ -34,6 +34,23 @@ import pack.ParticipantImp;
  */
 public class ProjectImp implements Project {
 
+    /**
+     * @param name
+     * @param descriptions
+     * @param numberOfFacilitators
+     * @param numberOfStudents
+     * @param numberOfPartners
+     * @param numberOfParticipants
+     * @param numberOfTasks
+     * @param maximumNumberOfTasks
+     * @param maximumNumberOfStudents
+     * @param maximumNumberOfPartners
+     * @param maximumNumberOfFacilitators
+     * @param numberOfTags
+     * @param tasks
+     * @param participants
+     * @param tags
+     */
     private String name, description;
     private int numberOfFacilitators, numberOfStudents, numberOfPartners, numberOfParticipants,
             numberOfTasks, maximumNumberOfTasks, maximumNumberOfStudents,
@@ -58,6 +75,17 @@ public class ProjectImp implements Project {
 //        this.participants = new Participant[(int) maximumNumberOfParticipants];
 //        this.tags = new String[2];
 //    }
+    
+    /**
+     * This is the constructor method of Project.
+     * @param name
+     * @param description
+     * @param numberOfFacilitators
+     * @param numberOfStudents
+     * @param numberOfPartners
+     * @param taskArraySize
+     * @param tags 
+     */
     public ProjectImp(String name, String description, int numberOfFacilitators, int numberOfStudents, int numberOfPartners, int taskArraySize, String[] tags) {
 
         this.name = name;
